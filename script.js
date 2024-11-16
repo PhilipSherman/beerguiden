fdf0ba3f50adf09a2265e92b2555249833d3c542
CarlONielsen <85550960+CarlONielsen@users.noreply.github.com>
2024-11-16 11:23:46 +0100

    Felj rettelse

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-filterSelection("all")
+filterSelection("all");
+
 function filterSelection(c) {
   var x, i;
   x = document.getElementsByClassName("filterDiv");
@@ -10,40 +11,36 @@ function filterSelection(c) {
 }
 
 function w3AddClass(element, name) {
-    var i, arr1, arr2;
-    arr1 = element.className.split(" ");
-    arr2 = name.split(" ");
-    for (i = 0; i < arr2.length; i++) {
-      if (arr1.indexOf(arr2[i]) == -1) {
-        element.className += " " + arr2[i];
-      }
+  var arr1 = element.className.split(" ");
+  var arr2 = name.split(" ");
+  for (var i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
     }
   }
+}
 
-  function w3RemoveClass(element, name) {
-    var i, arr1, arr2;
-    arr1 = element.className.split(" ");
-    arr2 = name.split(" ");
-    for (i = 0; i < arr2.length; i++) {
-      while (arr1.indexOf(arr2[i]) > -1) {
-        arr1.splice(arr1.indexOf(arr2[i]), 1);
-      }
+function w3RemoveClass(element, name) {
+  var arr1 = element.className.split(" ");
+  var arr2 = name.split(" ");
+  for (var i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
     }
-    element.className = arr1.join(" ");
   }
+  element.className = arr1.join(" ");
+}
 
-  var btnContainer = document.getElementById("ButtonContainer");
-  var btns = btnContainer.getElementsByClassName("btn");
-  
-  for (var i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", function() {
-      // Remove "active" class from the currently active button (if any)
-      var current = document.querySelector(".btn.active");
-      if (current) {
-        current.classList.remove("active");
-      }
-      // Add "active" class to the clicked button
-      this.classList.add("active");
-    });
-  }
+var btnContainer = document.getElementById("ButtonContainer");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    if (current.length > 0) {
+      current[0].className = current[0].className.replace(" active", "");
+    }
+    this.className += " active";
+  });
+}
+
   
